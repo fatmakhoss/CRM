efb7462e0954f6ab75ff1d126c083abffa0d437d
fatma <fatmakhoss9@gmail.com>
2024-06-26 13:37:22 +0200

    mon commit

--- app/Models/Contrats.php
+++ app/Models/Contrats.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Contrats extends Model
 {
     use HasFactory;
-    protected $fillable = [
+    protected $fillable = [ test
         'client_id', 'numero_contrat', 'date_debut', 'date_fin', 'details'
     ];
      // Relation avec le modèle Client
